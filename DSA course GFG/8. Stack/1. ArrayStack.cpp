5bdebe424939f662aee57656a82309ab8d41c83e
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2022-10-11 15:58:28 +0530

    StackBegin

---
+++ DSA course GFG/8. Stack/1. ArrayStack.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+class ArrStack{
+    private:
+        int *data;
+        int size = 0;
+        int cap = 0;
+
+    public:
+        ArrStack(){
+            cap = 10;
+            data = new int[cap];
+        }
+
+        ArrStack(int n){
+            cap = n;
+            data = new int[cap];
+
+        }
+
+        void push(int n){
+            if(size == cap){
+                return;
+            }
+            data[size] = n;
+            size++;
+        }
+
+        bool isEmpty(){
+            return (size == 0);
+        }
+
+        int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+
+            int temp = top();
+            size--;
+            return temp;
+        }
+
+        int top(){
+            if(isEmpty()){
+                return -1;
+            }
+
+            return data[size-1];
+        }
+
+        void display(){
+            for(int i=0; i<size; i++){
+                cout<<data[i]<<" ";
+            }
+            cout<<endl;
+        }
+};
+
+main(){
+    ArrStack s(5);
+     cout<<s.isEmpty()<<endl;
+    s.push(8);
+    s.push(3);
+    s.push(2);
+
+    cout<<s.top()<<endl;
+
+    s.pop();
+
+    cout<<s.top()<<endl;
+    cout<<s.isEmpty()<<endl;
+
+}
